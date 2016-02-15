6afadbb7a2be9342ddda982786be37de263e6328
Maxim Galaganov <voltage@lanport.net>
2016-02-16 00:36:15 +0300

    Fix indentation in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     errorHandler = require('errorhandler'),
     app = express(),
-	proxy = require('express-http-proxy');
+    proxy = require('express-http-proxy');
 
 var HOSTNAME = 'localhost',
     PORT = 8080,
@@ -10,25 +10,25 @@ var HOSTNAME = 'localhost',
 var count = 0;
 
 app.use(function (req, res, next) {
-	// Здесь нужно написать журналирование в формате
-	// (журналирование - вывод в консоль)
-	// [время] [номер запроса по счету]
+    // Здесь нужно написать журналирование в формате
+    // (журналирование - вывод в консоль)
+    // [время] [номер запроса по счету]
     console.log('[%s] [%s]', (new Date()).toLocaleString(), count++);
     next();
 });
 
 app
-	.use('/', express.static(PUBLIC_DIR))
-	.use(errorHandler());
+    .use('/', express.static(PUBLIC_DIR))
+    .use(errorHandler());
 
 app.listen(PORT, function () {
-	console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
+    console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
 });
 
 
 app.use('/proxy', proxy('http://vk.com', {
-	forwardPath: function(req, res) {
-		console.log(1234);
-		return require('url').parse(req.url).path;
-	}
+    forwardPath: function (req, res) {
+        console.log(1234);
+        return require('url').parse(req.url).path;
+    }
 }));
