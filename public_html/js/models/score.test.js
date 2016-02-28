8bfb207f3538b20fec1cb71a5c8d4742c8945721
Artem Ustimov <art@ustimov.org>
2016-02-28 15:03:27 +0300

    Model and tests

--- public_html/js/models/score.test.js
+++ public_html/js/models/score.test.js
@@ -9,4 +9,20 @@ define(function (require) {
         QUnit.ok(score instanceof Backbone.Model);
 
     });
+
+    QUnit.test("ScoreModel - корректность значений по умолчанию", function () {
+        var ScoreModel = require('./score'),
+            score = new ScoreModel();
+
+        QUnit.equal(score.get('name'), '', 'Некорректное имя по умолчанию');
+        QUnit.equal(score.get('score'), 0, 'Некорректный счёт по умолчанию');
+    });
+
+    QUnit.test("ScoreModel - корректность установки значений", function () {
+        var ScoreModel = require('./score'),
+            score = new ScoreModel({name: 'Artem', score: 1337});
+
+        QUnit.equal(score.get('name'), 'Artem', 'Имя установлено некорректно');
+        QUnit.equal(score.get('score'), 1337, 'Имя установлено некорректно');
+    });
 });
