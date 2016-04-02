3356644e2ac525fa2d0df234da88e3f030ac2d1a
Artem Ustimov <art@ustimov.org>
2016-04-02 20:26:18 +0300

    Timeout before rendering

--- public_html/js/game/game.js
+++ public_html/js/game/game.js
@@ -14,22 +14,22 @@ define(function (require) {
         var image = new Image();
         image.src = '/images/rx4.png';
         initTile.setContent(new Card(0, 0, 100, 100, false, image));
-        image.onload = function () {
-            offScreenRenderer.render();
-            screenRenderer.render();
-        };
+
         var allValid = [1, 2, 3, 4];
         table.getTile(16 * 34 + 15).setValid(allValid, allValid, allValid);
         table.getTile(16 * 34 + 17).setValid(allValid, allValid, allValid);
         table.getTile(15 * 34 + 16).setValid(allValid, allValid, allValid);
         table.getTile(17 * 34 + 16).setValid(allValid, allValid, allValid);
         offScreenRenderer.addDrawable(table);
-        offScreenRenderer.render();
-        console.log(document.getElementById('canvas'));
+
         var screenRenderer = new ScreenRenderer(document.getElementById('canvas'),
             new Camera(offScreenCanvas, TABLE_SIZE / 2 - window.innerWidth / 2,
                 TABLE_SIZE / 2 - window.innerHeight / 2, window.innerWidth, window.innerHeight),
             table, offScreenRenderer, window.innerWidth, window.innerHeight);
-        screenRenderer.render();
+
+        setTimeout(function () {
+            offScreenRenderer.render();
+            screenRenderer.render();
+        }, 1000);
     };
 });
