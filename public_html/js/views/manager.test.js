5c089f5c24ade0963a19775ae3b25c689a503689
Artem Ustimov <art@ustimov.org>
2016-04-03 12:15:53 +0300

    View manager tests

---
+++ public_html/js/views/manager.test.js
@@ -0,0 +1,42 @@
+define(function (require) {
+
+    QUnit.module("views/manager");
+
+    QUnit.test("ViewManager - экземпляр Backbone.View", function () {
+
+        var ViewManager = require('./manager'),
+            viewManager = new ViewManager();
+
+        QUnit.ok(viewManager instanceof Backbone.View);
+
+    });
+
+    QUnit.test("ViewManager - тестирование логики", function () {
+
+        var ViewManager = require('./manager'),
+            LoginView = require('./login'),
+            RegView = require('./reg'),
+            $ = require('jquery');
+
+        var views = {
+                login: new LoginView(),
+                reg: new RegView()
+            },
+            viewManager = new ViewManager(views);
+
+        QUnit.equal(views['login'].$el.css('display') === 'none', true);
+        QUnit.equal(views['reg'].$el.css('display') === 'none', true);
+
+        views['login'].show();
+
+        QUnit.equal(views['login'].$el.css('display') === 'none', false);
+        QUnit.equal(views['reg'].$el.css('display') === 'none', true);
+
+        views['reg'].show();
+
+        QUnit.equal(views['login'].$el.css('display') === 'none', true);
+        QUnit.equal(views['reg'].$el.css('display') === 'none', false);
+
+    });
+
+});
