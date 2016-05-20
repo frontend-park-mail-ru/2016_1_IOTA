5bafcbc1f9cdaa6e74d0e7eb871a771972759b7a
Maxim Galaganov <voltage@lanport.net>
2016-05-21 00:44:23 +0300

    Fix proxy to support web socket

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     errorHandler = require('errorhandler'),
     app = express(),
-    proxy = require('express-http-proxy');
+    proxy = require('http-proxy-middleware');
 
 var HOSTNAME = 'localhost',
     PORT = 8000,
@@ -25,10 +25,4 @@ app.listen(PORT, function () {
     console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
 });
 
-app.use('/api', proxy('http://127.0.0.1:8080/', {
-    forwardPath: function (req, res) {
-        var url = '/api' + require('url').parse(req.url).path;
-        console.log('Proxy: ' + url);
-        return url;
-    }
-}));
+app.use('/api', proxy('http://127.0.0.1:8080/', {ws: true}));
