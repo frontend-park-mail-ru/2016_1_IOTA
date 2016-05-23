16c4d62e909d870c927489b85b25396928ac4819
Artem Ustimov <art@ustimov.org>
2016-05-23 20:15:16 +0300

    GET to /api/game after Start Game button click

--- public_html/js/models/game.js
+++ public_html/js/models/game.js
@@ -1,33 +1,16 @@
 define(function (require) {
 
-    var Backbone = require('backbone'),
-        socket = require('models/ws');
+    var Backbone = require('backbone');
 
     //noinspection UnnecessaryLocalVariableJS
     var GameModel = Backbone.Model.extend({
 
-        initialize: function () {
-            this.listenTo(socket, 'message', function (data) {
-                console.log(data);
-                data = JSON.parse(data);
-                if (!data.__ok || !data.payload) {
-                    return;
-                }
-                console.log('Обновление модели');
-                this.set(data.payload);
-            });
-        },
+        url: '/api/game'
 
-        start: function () {
-            socket.send(JSON.stringify({ready: true}));
-        },
-
-        sync: function(method, model, options) {
-            switch(method) {
-                case 'update':
-                    socket.send(model.toJSON());
-            }
-        }
+        // defaults: {
+        //     id: -1,
+        // },
+        
     });
 
     return new GameModel();
