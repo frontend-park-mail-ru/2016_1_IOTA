b94071b5ee0fdf523c1054da7580fe8c94ac0f1d
Artem Ustimov <art@ustimov.org>
2016-06-03 21:49:38 +0300

    Canvas score

---
+++ public_html/js/game/score.js
@@ -0,0 +1,46 @@
+define(function (require) {
+
+    var __extends = require('./extends'),
+        Sprite = require('./sprite');
+
+    //noinspection UnnecessaryLocalVariableJS
+    var Score = (function (_super) {
+
+        __extends(Score, _super);
+
+        function Score(x, y, width, height, name, score) {
+            _super.call(this, x, y, width, height);
+            this.name = name;
+            this.score = score;
+        }
+
+        Score.prototype.update = function (name, score, isTurnPlayer, isCurrentPlayer) {
+            this.name = name;
+            this.score = score;
+            this.isTurnPlayer = isTurnPlayer;
+            this.isCurrentPlayer = isCurrentPlayer;
+        };
+
+        Score.prototype.draw = function (canvas) {
+            if (!this.name) {
+                return;
+            }
+            var context = canvas.getContext('2d');
+            context.font = "18px Verdana";
+            if (this.isCurrentPlayer) {
+                context.font = "bold " + context.font;
+            }
+            var printName = this.name;
+            if (this.isTurnPlayer) {
+                printName = "\u00BB " + printName;
+            }
+            context.fillText(printName + ': ' + this.score, this.x, this.y, this.width);
+        };
+
+        return Score;
+
+    }(Sprite));
+
+    return Score;
+
+});
